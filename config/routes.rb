2bb4ebaf62a6a7a3813e8945558c3452e2d7d5f8
emiliaharaszczuk <emilia.haraszczuk@gmail.com>
2017-11-16 14:53:29 +0000

    Matching functionality to user role; Cart quantity info in menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  get 'cart/index'
+  #get 'cart/index'
 
   resources :products
   resources :categories
   root 'static_pages#home'
 
-  
-  #get 'static_pages/home'
-  #get 'static_pages/help'
-  #get 'static_pages/about'
-  
-  #get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   
@@ -21,8 +15,6 @@ Rails.application.routes.draw do
   get 'products/index'
   get 'categories/index'
   
-  #get '/Admin' => 'user#admin_login'
-  #get 'logout' => 'user#logout'
   get '/admin', to: 'user#admin_login'
   get '/logout', to: 'user#logout'
   
@@ -30,8 +22,6 @@ Rails.application.routes.draw do
   get '/cart/clear' => 'cart#clearCart'
   get '/cart/:id' => 'cart#add'
   get '/cart/remove/:id' => 'cart#remove'
-  
-  #root :to => 'site#home'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
